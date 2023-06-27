75c0e1be5172c38e0002496a0049182b1263458e
Fedora <fedora@192.168.1.10>
2023-06-27 12:23:56 -0500

    modified script and html

--- script.js
+++ script.js
@@ -79,7 +79,7 @@ $(document).ready(function () {
     scrollBar: true,
     responsiveWidth: 500,
     navigation: true,
-    navigationTooltips: ["home", "about", "qualification","portfolio", "contact", "connect"],
+    navigationTooltips: ["home", "about", "qualification", "portfolio", "contact", "connect"],
     anchors: ["home", "about", "qualification", "portfolio", "contact", "connect"],
     menu: "#myMenu",
     fitToSection: true,
@@ -142,21 +142,21 @@ $(document).ready(function () {
   $(document).on("click", "#contact", function () {
     $.fn.fullpage.moveTo(5);
   });
-  
+
   $(document).on("click", "#connect", function () {
     $.fn.fullpage.moveTo(6);
   });
-  
+
   $(document).on("click", "#home", function () {
     $.fn.fullpage.moveTo(1);
   });
-  
+
   // smooth scrolling
   $(function () {
     $("a[href*=#]:not([href=#])").click(function () {
       if (
         location.pathname.replace(/^\//, "") ==
-          this.pathname.replace(/^\//, "") &&
+        this.pathname.replace(/^\//, "") &&
         location.hostname == this.hostname
       ) {
         var target = $(this.hash);
@@ -180,7 +180,6 @@ $(document).ready(function () {
   $(function () {
     // Get the form.
     var form = $("#ajax-contact");
-
     // Get the messages div.
     var formMessages = $("#form-messages");
 
@@ -189,14 +188,86 @@ $(document).ready(function () {
       // Stop the browser from submitting the form.
       e.preventDefault();
 
+      $(formMessages).text(
+        "Wait please, sending your message!"
+      );
+
+      var apiKey = '1477932a948ac179ca35c0462529d557';
+      var endpoint = 'https://api.mailjet.com/v3.1/send';
+      // Serialize the form data.
+      var formData = $(form).serialize();
+
+      var emailData = {
+        Messages: [
+          {
+            From: {
+              Email: "inghector1991@outlook.com",
+              Name: "Hector Carreno"
+            },
+            To: [
+              {
+                Email: 'inghector1991@outlook.com'
+              }
+            ],
+            Subject: 'ContactMe - HC Tech development | My Web site',
+            HTMLPart: '<html><body><h1>Hello, recipient!</h1><p>This is the body of the email.</p><p>Name: ' + "Name" + '</p><p>Email: ' + "formData.Email" + '</p><p>Message: ' + "formData.Messages" + '</p></body></html>'
+          }
+        ]
+      };
+
+      // fetch(endpoint, {
+      //   method: 'POST',
+      //   headers: {
+      //     'Authorization': 'Basic ' + apiKey,
+      //     'Content-Type': 'application/json'
+      //   },
+      //   body: JSON.stringify(emailData)
+      // })
+      //   .then(function (response) {
+      //     if (response.ok) {
+      //       // Make sure that the formMessages div has the 'success' class.
+      //       $(formMessages).addClass("success");
+      //       $(formMessages).removeClass("error");
+
+      //       // Set the message text.
+      //       $(formMessages).text(
+      //         "Message sent!"
+      //       );
+
+      //       // Clear the form.
+      //       $("#name").val("");
+      //       $("#email").val("");
+      //       $("#message").val("");
+
+      //     } else {
+      //       // Make sure that the formMessages div has the 'error' class.
+      //       $(formMessages).removeClass("success");
+      //       $(formMessages).addClass("error");
+
+      //       $(formMessages).text(
+      //         "Oops! An error occured and your message could not be sent. (" + response.response + ")"
+      //       );
+      //     }
+      //   })
+      //   .catch(function (error) {
+      //     $(formMessages).text(
+      //       "Oops! A crash has occured and your message could not be sent. (" + error.response + ")"
+      //     );
+      //   });
+
       // Serialize the form data.
       var formData = $(form).serialize();
 
       // Submit the form using AJAX.
       $.ajax({
         type: "POST",
-        url: $(form).attr("action"),
-        data: formData
+        url: endpoint,
+        data: emailData,
+        headers: {
+          'Authorization': 'Basic ' + apiKey,
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(emailData)
       })
         .done(function (response) {
           // Make sure that the formMessages div has the 'success' class.
@@ -210,20 +281,26 @@ $(document).ready(function () {
           $("#name").val("");
           $("#email").val("");
           $("#message").val("");
+          // Send the email
+          // sendEmail(formData);  
         })
         .fail(function (data) {
           // Make sure that the formMessages div has the 'error' class.
           $(formMessages).removeClass("success");
           $(formMessages).addClass("error");
 
+          $(formMessages).text(
+            "Oops! An error occured and your message could not be sent."
+          );
+
           // Set the message text.
-          if (data.responseText !== "") {
-            $(formMessages).text(data.responseText);
-          } else {
-            $(formMessages).text(
-              "Oops! An error occured and your message could not be sent."
-            );
-          }
+          // if (data.responseText != "") {
+          //   $(formMessages).text(data.responseText);
+          // } else {
+          //   $(formMessages).text(
+          //     "Oops! An error occured and your message could not be sent."
+          //   );
+          // }
         });
     });
   });
